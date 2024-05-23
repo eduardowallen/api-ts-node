eac60d6609fb8759f6f1429990e4106c3b27fd67
Eduardo Wallén <eduardo.wallen@afry.com>
2024-05-23 15:40:35 +0200

    Added Joi to replace manual validation

--- src/utils/users.validators.ts
+++ src/utils/users.validators.ts
@@ -3,7 +3,37 @@ import {
     Response,
     NextFunction
 } from 'express'
+import Joi, {
+    ValidationResult
+} from 'joi'
 
+export const userSchema = Joi.object({
+    id: Joi.number().integer().required(),
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required().min(8)
+})
+
+export const validateUser = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    // Validate user data
+    const result: ValidationResult = 
+        userSchema.validate(
+            req.body,
+            { abortEarly: false } // Return all errors
+        )
+        if (result.error) {
+            return res.status(422).json({
+                message: 'Invalid request data',
+                errors: result.error.details.map(err => err.message)
+            })
+        }
+    next()
+}
+/*
 export const validateUser = (
     req: Request,
     res: Response,
@@ -51,3 +81,4 @@ export const validateUser = (
     // Pass user data to the next middleware
     next()
 }
+*/
