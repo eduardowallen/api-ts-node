f3b8b0a1b66823ebc1df1b32cc36f1748ed7ef35
Eduardo Wallén <eduardo.wallen@afry.com>
2024-05-28 11:24:31 +0200

    Environmental variables can now be read from env file

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,17 @@ import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import router from './routes'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
-const port = 3000
+const env = process.env['APP_ENV'] || 'dev'
+const port = process.env['APP_PORT'] || 3000
 
 app.use(cors())
 app.use(helmet())
-app.use(morgan('dev'))
+app.use(morgan(env))
 app.use(express.json())
 
 app.use('/', router)
