6df4d985efc542158935e6861e92c3c74745f8ca
Eduardo Wallén <eduardo.wallen@gmail.com>
2024-05-30 18:56:13 +0200

    Create database if it does not exist

---
+++ src/config/dbsetup.ts
@@ -0,0 +1,7 @@
+import mysql from 'mysql2/promise'
+import { CREATE_DATABASE, CREATE_USERS_TABLE, CREATE_TASKS_TABLE} from './dbqueries'
+export default async function setupDatabase(connection: mysql.Connection): Promise<void> {
+    await connection.query(CREATE_DATABASE)
+    await connection.query(CREATE_USERS_TABLE)
+    await connection.query(CREATE_TASKS_TABLE)
+}
--- src/config/poolManager.ts
+++ src/config/poolManager.ts
@@ -1,11 +1,12 @@
 import mysql from 'mysql2/promise'
+import setupDatabase from './dbsetup'
 
 class PoolManager {
     private static instance: mysql.Pool
 
     private constructor() {}
 
-    public static getInstance(): mysql.Pool {
+    public static async getInstance(): Promise<mysql.Pool> {
         if (!PoolManager.instance) {
             PoolManager.instance = mysql.createPool({
                 host: process.env.MYSQL_HOST,
@@ -18,6 +19,22 @@ class PoolManager {
             })
         }
         return PoolManager.instance
+    }
+    public static async checkDatabaseExists(): Promise<void> {
+        const connection = await mysql.createConnection({
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD
+        })
+
+        const [rows] = await connection.query(`SHOW DATABASES LIKE '${process.env.MYSQL_DATABASE}'`)
+        if ((rows as any).length === 0) {
+            await setupDatabase(connection)
+            console.log(`Database missing. Setting up database '${process.env.MYSQL_DATABASE}'`)
+        } else {
+            console.log('Database exists, skipping setup')
+        }
+        await connection.end()
     }
 }
 
