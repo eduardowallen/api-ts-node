bc75fa75a11b5dc1d9c86c0eae4fe8e40f7d0fb9
Eduardo Wallén <eduardo.wallen@gmail.com>
2024-06-18 22:54:59 +0200

    WIP database changes

--- src/config/dbqueries.ts
+++ src/config/dbqueries.ts
@@ -3,21 +3,78 @@ dotenv.config()
 
 export const CREATE_DATABASE = `CREATE DATABASE IF NOT EXISTS ${process.env.MYSQL_DATABASE}`
 export const SELECT_DATABASE = `USE ${process.env.MYSQL_DATABASE}`
-export const CREATE_USERS_TABLE = `CREATE TABLE IF NOT EXISTS users (
+
+const default_user_interface = `(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`
+
+/* Users tables */
+export const CREATE_GLOBALADMINS_TABLE = `CREATE TABLE IF NOT EXISTS globaladmins` + {default_user_interface} + `)`
+export const CREATE_VISITORS_TABLE = `CREATE TABLE IF NOT EXISTS visitors` + {default_user_interface} + `)`
+export const CREATE_ORGANIZERS_TABLE = `CREATE TABLE IF NOT EXISTS organizers` + {default_user_interface} + `)`
+export const CREATE_ADMINS_TABLE = `CREATE TABLE IF NOT EXISTS admins` + {default_user_interface} + `)`
+export const CREATE_EXHIBITORS_TABLE = `CREATE TABLE IF NOT EXISTS exhibitors` + {default_user_interface} + `)`
+export const CREATE_SPEAKERS_TABLE = `CREATE TABLE IF NOT EXISTS speakers` + {default_user_interface} + `)`
+
+/* Events, maps, spaces and bookings table */
+export const CREATE_EVENTS_TABLE = `CREATE TABLE IF NOT EXISTS events (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    sort_order INT DEFAULT 0,
+    owner_id INT NOT NULL,
+    created_by INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)`
+export const CREATE_MAPS_TABLE = `CREATE TABLE IF NOT EXISTS maps (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    sort_order INT DEFAULT 0,
+    owner_id INT NOT NULL,
+    created_by INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)`
+export const CREATE_BOOKINGS_TABLE = `CREATE TABLE IF NOT EXISTS bookings (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    exhibitor_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )`
 
-export const CREATE_TASKS_TABLE = `CREATE TABLE IF NOT EXISTS tasks (
+/* Services tables */
+export const CREATE_INVOICES_TABLE = `CREATE TABLE IF NOT EXISTS invoices (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     sort_order INT DEFAULT 0,
     user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)`
+export const CREATE_SMS_TABLE = `CREATE TABLE IF NOT EXISTS sms (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    sort_order INT DEFAULT 0,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)`
+export const CREATE_TRANSLATIONS_TABLE = `CREATE TABLE IF NOT EXISTS translations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    system_string VARCHAR(255) NOT NULL,
+    english VARCHAR(255) NOT NULL,
+    swedish VARCHAR(255) NOT NULL,
+    sort_order INT DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )`
--- src/config/dbsetup.ts
+++ src/config/dbsetup.ts
@@ -1,9 +1,22 @@
 import mysql from 'mysql2/promise'
-import { CREATE_DATABASE, SELECT_DATABASE, CREATE_USERS_TABLE, CREATE_TASKS_TABLE} from './dbqueries'
+import { CREATE_DATABASE, SELECT_DATABASE, 
+    CREATE_GLOBALADMINS_TABLE, CREATE_ORGANIZERS_TABLE, CREATE_ADMINS_TABLE, CREATE_EXHIBITORS_TABLE, CREATE_SPEAKERS_TABLE, CREATE_VISITORS_TABLE, 
+    CREATE_EVENTS_TABLE, CREATE_MAPS_TABLE, CREATE_BOOKINGS_TABLE, 
+    CREATE_INVOICES_TABLE, CREATE_SMS_TABLE, CREATE_TRANSLATIONS_TABLE } from './dbqueries'
 export default async function setupDatabase(connection: mysql.Connection): Promise<void> {
     await connection.query(CREATE_DATABASE)
     await connection.query(SELECT_DATABASE)
-    await connection.query(CREATE_USERS_TABLE)
-    await connection.query(CREATE_TASKS_TABLE)
+    await connection.query(CREATE_GLOBALADMINS_TABLE)
+    await connection.query(CREATE_ORGANIZERS_TABLE)
+    await connection.query(CREATE_ADMINS_TABLE)
+    await connection.query(CREATE_EXHIBITORS_TABLE)
+    await connection.query(CREATE_SPEAKERS_TABLE)
+    await connection.query(CREATE_VISITORS_TABLE)
+    await connection.query(CREATE_EVENTS_TABLE)
+    await connection.query(CREATE_MAPS_TABLE)
+    await connection.query(CREATE_BOOKINGS_TABLE)
+    await connection.query(CREATE_INVOICES_TABLE)
+    await connection.query(CREATE_SMS_TABLE)
+    await connection.query(CREATE_TRANSLATIONS_TABLE)
     return Promise.resolve()
 }
